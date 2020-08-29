8fe9543f6500f8e1b65265e938922d54ad157fd4
Pete <38692750+PavelRasiukevich@users.noreply.github.com>
2020-08-30 01:39:27 +0300

    almost fixed wrong behaviour

--- project/Assets/FallingRock.cs
+++ project/Assets/FallingRock.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class FallingRock : MonoBehaviour
 {
+    public float fallingSpeed;
+    public bool isFalling;
+   
+
+    private void Update()
+    {
+        if (isFalling)
+        {
+            transform.Translate(Vector2.up * fallingSpeed * Time.deltaTime * -1);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<FallingRockTrigger>())
+        if (collision.gameObject.CompareTag("Landing Ground"))
         {
             Destroy(gameObject);
         }
